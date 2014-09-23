f834b9898e4aa58ad639c2c2ba83a00a12db55c7
Paolo Ruiz <pjruiz15@cs.utexas.edu>
2014-09-22 20:37:47 -0500

    Fixing some values and getting new scores.

--- fantasy/singleSF.py
+++ fantasy/singleSF.py
@@ -5,12 +5,12 @@ from nbaplayer import NbaPlayer
 from score import Score
 year = 2013
 se = Season()
-sortedPlayers = se.calcSeason('season/thisStats.txt', year, 3, 1, 2, 28, 23, 22, 24, 25, 26, 10, 19, 11)
+sortedPlayers = se.calcSeason('season/thisStats.txt', year, 3, 1, 2, 28, 23, 22, 24, 25, 26, 10, 19, 11, 7)
 scores = []
 for p in sortedPlayers:
   se = p.getSeason(year)
   if ("SF" in p.position):
-    scores.append(Score(p.name, se.zScore(0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0), se.zScore()))
+    scores.append(Score(p.name, se.zScore(1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0), se.zScore()))
 scores = sorted(scores)
 for j in scores:
   print(j.name + "\t" + str(j.score) + "\t" + str(j.extra))
