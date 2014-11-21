94ab069ff79194fc1059508ed5faf16b435397e0
Paolo Ruiz <pjruiz15@cs.utexas.edu>
2014-11-20 22:50:43 -0600

    Adding functionality to work with how losses are categorized.

--- stats/pageRankGames.py
+++ stats/pageRankGames.py
@@ -40,11 +40,16 @@ for line in f:
       losses.append((home, abs(diff)))
       teamLosses[away] = losses
 teams = teamRanks.keys()
-max_iterations = 100
+max_iterations = 500
 damping = 0.85
 min_delta = 0.0000001
 
+
+closeGameMax = 5
+midGameMax = 11
 #switch to not using powers, and instead considering 1-4 point wins worth a certain amount, 5-10 worth a certain amount of the whole and so on
+
+#Not penalizing enough for losses
 for i in range(max_iterations):
   diff = 0
   for node in teams:
@@ -54,19 +59,33 @@ for i in range(max_iterations):
       defeatedTeams = teamWins[node]
     for defeated in defeatedTeams:
       lossScore = 0.0
-      lossCount = 0
+      #
+      scoreUnit = 2.0
+      if defeated[1] <= closeGameMax:
+        scoreUnit = 1.0
+      elif defeated[1] <= midGameMax:
+        scoreUnit = 1.5
+      #
       tLosses = []
       if defeated[0] in teamLosses:
         tLosses = teamLosses[defeated[0]]
       for loss in tLosses:
-        lossScore += math.sqrt(loss[1])
-        lossCount += 1
-      rank += damping * teamRanks[defeated[0]] * math.sqrt(defeated[1]) / lossScore
+        #lossScore += math.sqrt(loss[1])
+        if defeated[1] <= closeGameMax:
+          lossScore += 1.0
+        elif defeated[1] <= midGameMax:
+          lossScore += 1.5
+        else:
+          lossScore += 2.0
+        #
+      #rank += damping * teamRanks[defeated[0]] * math.sqrt(defeated[1]) / lossScore
+      rank += damping * teamRanks[defeated[0]] * scoreUnit / lossScore
     diff += abs(teamRanks[node] - rank)
     teamRanks[node] = rank
   if diff < min_delta:
     break
 sortedRanks = sorted(teamRanks.items(), key=operator.itemgetter(1))
-for team in sortedRanks:
-  print team[0]
-  print team[1]*7500
+print sortedRanks
+#for team in sortedRanks:
+#  print team[0]
+#  print team[1]*7500
